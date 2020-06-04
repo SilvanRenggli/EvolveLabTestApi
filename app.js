7013e944e77fc393ccb28747b0f5a137e1ac070b
Silvan Renggli <rengglsi@student.ethz.ch>
2020-06-04 22:14:51 +0200

    added enemy kills and winrate and updates depth if neccessary

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.post("/create_creature", async (req, res) => {
-    console.log("called")
     try{
         const creature = new Creature(req.body);
         await creature.save();
@@ -20,6 +19,35 @@ app.post("/create_creature", async (req, res) => {
     }
 });
 
+app.post("/update_enemy", async (req, res) => {
+    console.log("called_update_enemy")
+    try{
+        const id = re.body["id"];
+        const enemy = Creature.findById(id);
+        var winrate = enemy["winrate"];
+        var kills = enemy["kills"];
+        var depth = enemy["depth"];
+        if (req.body["won"]){
+            winrate += 1;
+            await Creature.update({"_id" : id},{$set: {"kills": kills + 1}})
+            if(winrate > 2){
+                await Creature.update(
+                    {"_id" : id}, 
+                    {$set: {"depth" : depth + 1, "winrate": 0 }});
+            }
+        }else{
+            winrate -= 1;
+            if(winrate < -2){
+                await Creature.update(
+                    {"_id" : id}, 
+                    {$set: {"depth" : max(depth -1, 0), "winrate": 0 }});
+            }
+        }
+    } catch(err){
+        res.send({message: err})
+        console.log(err)
+    }
+});
 
 app.get("/load_creature", async (req, res) => {
 

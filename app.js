19eb1dcf25e6f38ed2f467ad898c8918deda346d
Silvan Renggli <rengglsi@student.ethz.ch>
2020-06-05 12:17:39 +0200

    minor fixes

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/calc_user_score", async (req, res) => {
                 _id : "$owner",
                 deepest : {$max : "$depth"},
                 kills: {$sum : "kills"},
-                score: {$sum : {$multiply :["$kills", "$depth" + 1]}}
+                score: {$sum : {$multiply :["$depth", {$add: ["$kills", {"$literal" : 1}]}]}}
         }}
         ])
         .exec()

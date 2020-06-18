ea8e233b02361d5b8e56c69be66831272aca815a
Silvan Renggli <rengglsi@student.ethz.ch>
2020-06-18 18:11:06 +0200

    added function to load an enemy with a crystal

--- app.js
+++ app.js
@@ -120,6 +120,24 @@ app.get("/load_creature", async (req, res) => {
   })
 })
 
+app.get("/load_crystall_creature", async (req, res) => {
+
+    var depth = req.body["depth"];
+    Creature.find({depth: depth},{crystalls: { $gt: 0}}).countDocuments().exec(function (err, count) {
+
+    // Get a random entry
+    var random = Math.floor(Math.random() * count)
+  
+    Creature.findOne({depth: depth}).skip(random)
+    .exec()
+    .then(doc => {
+        console.log(doc)
+        res.status(200).json(doc);
+    })
+    .catch(err)
+  })
+})
+
 mongoose.connect(process.env.DB_CONNECTION_STRING,
                 { useNewUrlParser: true, useUnifiedTopology: true },(req, res) =>{
     console.log("Connected to the database")

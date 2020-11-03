d5e4f8c6831d60397fc94657b2117e52c3e529a2
Silvan Renggli <rengglsi@student.ethz.ch>
2020-11-03 19:42:36 +0100

    minor adjustments

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/login', async (req, res) => {
 app.post('/token', async (req,res) => {
     const refreshToken = req.body.token
     if (refreshToken == null) return res.sendStatus(401)
-    const token = await User.findOne({token: refreshToken})
+    const token = await Token.findOne({token: refreshToken})
     if (token == null) return res.sendStatus(403)
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
         if (err) return res.sendStatus(403)

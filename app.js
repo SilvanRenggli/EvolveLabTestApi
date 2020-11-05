a04ef32b52eacda3c6213da33e9632f77234d641
Silvan Renggli <rengglsi@student.ethz.ch>
2020-11-05 16:02:05 +0100

    minor adjustments

--- app.js
+++ app.js
@@ -64,11 +64,9 @@ app.post("/store_creature", authenticateToken, async (req, res) => {
 
 app.post("/create_user", async (req, res) => {
     //registers a new user
-    const user = new User()
+    const user = new User(req.body)
     try {
         user.password = await bcrypt.hash(req.body.password, 10)
-        user.name = rec.body.name
-        user.email = rec.body.email
         await user.save()
         res.status(201).send(user)
     }catch{

349afb6f86c68972121e3bbc6d012fd5f40f47da
Silvan Renggli <rengglsi@student.ethz.ch>
2020-11-05 15:43:28 +0100

    minor adjustments

--- model/user.js
+++ model/user.js
@@ -1,28 +1,25 @@
 const mongoose = require("mongoose");
 
 const User = new mongoose.Schema({
-    // name: {
-    //     type: String,
-    //     unique: true,
-    //     required: true,
-    //     maxlength: 30,
-    //     minlength: 1
-    // },
-    // email: {
-    //     type: String,
-    //     required: true,
-    //     maxlength: 320,
-    //     minlength: 3
-    // },
-    // password: {
-    //     type: String,
-    //     required: true,
-    //     maxlength: 30,
-    //     minlength: 6
-    // }
-    name: String,
-    email: String,
-    password: String
+    name: {
+        type: String,
+        //unique: true,
+        // required: true,
+        // maxlength: 30,
+        // minlength: 1
+    },
+    email: {
+        type: String,
+        // required: true,
+        // maxlength: 320,
+        // minlength: 3
+    },
+    password: {
+        type: String,
+        // required: true,
+        // maxlength: 30,
+        // minlength: 6
+    }
 })
 
 module.exports = mongoose.model("users", User)
